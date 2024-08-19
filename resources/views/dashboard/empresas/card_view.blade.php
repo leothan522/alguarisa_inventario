1ce51ead01e9cc0dd4e393827bcd9e89c5d9417a
Yonathan Castillo <leothan522@gmail.com>
2024-08-19 19:31:48 -0400

    Ekko Lightbox | Mejorado Responsive en Empresas

--- resources/views/dashboard/empresas/card_view.blade.php
+++ resources/views/dashboard/empresas/card_view.blade.php
@@ -1,13 +1,16 @@
-<div class="card {{--card-outline--}} card-navy" xmlns:wire="http://www.w3.org/1999/xhtml">
+<div class="card card-navy d-none d-sm-block" xmlns:wire="http://www.w3.org/1999/xhtml">
     <div class="card-header">
         <h3 class="card-title">
             {{ $title }}
         </h3>
 
         <div class="card-tools">
+            <button type="button" class="btn btn-tool" wire:click="actualizar">
+                <i class="fas fa-sync-alt"></i>
+            </button>
             @if($nuevo)
                 <button type="button" class="btn btn-tool" wire:click="create" @if(!comprobarPermisos('empresas.create')) disabled @endif>
-                    <i class="fas fa-file"></i> Nueva Tienda
+                    <i class="fas fa-file"></i> Nuevo
                 </button>
             @endif
             @if($btn_cancelar)
@@ -28,6 +31,12 @@
         <div class="card-footer text-center @if(!comprobarAccesoEmpresa($permisos, auth()->id())) d-none @endif">
 
             @if(!$verDefault)
+                @if(auth()->user()->role == 100)
+                    <button type="button" class="btn btn-default btn-sm mr-1" wire:click="destroy({{ $empresas_id }})"
+                            @if(!comprobarPermisos('empresas.destroy')) disabled @endif>
+                        <i class="fas fa-trash-alt"></i> Borrar Empresa
+                    </button>
+                @endif
                 <button type="button" class="btn btn-default btn-sm mr-1"
                         wire:click="convertirDefault({{ $empresas_id }})"
                         @if(!comprobarPermisos('empresas.edit')) disabled @endif>
@@ -42,20 +51,14 @@
 
             <button type="button" class="btn btn-default btn-sm" wire:click="edit"
                     @if(!comprobarPermisos('empresas.edit')) disabled @endif>
-                <i class="fas fa-edit"></i> {{ __('Edit') }} Información
+                <i class="fas fa-edit"></i> Editar Información
             </button>
-                {{--@if(!$verDefault)
-                    <button type="button" class="btn btn-default btn-sm" wire:click="destroy({{ $empresas_id }})"
-                            @if(!comprobarPermisos('empresas.destroy')) disabled @endif>
-                        <i class="fas fa-trash-alt"></i> Borrar Empresa
-                    </button>
-                @endif--}}
 
         </div>
     @endif
 
     <div class="overlay-wrapper" wire:loading wire:target="limpiar, create, show, save, edit, convertirDefault,
-                destroy, verHorario, botonHorario, diasActivos, storeHoras">
+                destroy, verHorario, botonHorario, {{--diasActivos,--}} storeHoras, actualizar">
         <div class="overlay">
             <div class="spinner-border text-navy" role="status">
                 <span class="sr-only">Loading...</span>
